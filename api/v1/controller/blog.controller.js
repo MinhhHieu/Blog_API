9fb6aa8c43ce353131f0bd91bc8b12a14140e266
Minh Hieu <minhhieudoqb255@gmail.com>
2025-07-12 10:36:56 +0700

    search

--- api/v1/controller/blog.controller.js
+++ api/v1/controller/blog.controller.js
@@ -1,11 +1,28 @@
 const Blog = require("../model/blog.model");
 
+const searchHelper = require("../../../helpers/search");
+
 module.exports.index = async (req, res) => {
+  try {
     const find = {
-        deleted: false
+      deleted: false,
     };
+
+    // Search 
+    let objectSearch = searchHelper(req.query);
+
+    if (req.query.keyword) {
+        find.title = objectSearch.regex;
+    }
+    // End Search
 
     const blogs = await Blog.find(find);
 
     res.json(blogs);
-}
+  } catch (error) {
+    res.json({
+        code: 400,
+        message: "Error"
+    });
+  }
+};
